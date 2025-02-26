0da9efeb6dd560259b0b42fb6aecffd1348496ba
Anthony George <anthony.george@deakin.edu.au>
2025-02-27 09:36:03 +1100

    feat: add tsconfig-paths

--- src/lib/env/fileLoader/tsFileLoader.ts
+++ src/lib/env/fileLoader/tsFileLoader.ts
@@ -1,16 +1,18 @@
 import { FileLoader, Migration } from './fileLoader';
 import * as paths from '../paths';
 import { pathToFileURL } from 'url';
+import { register } from 'ts-node';
 
 export class TsFileLoader extends FileLoader {
     constructor() {
         super(paths.tsExtension, paths.tsMigrationPath);
         // Ensure ts-node is registered
-        require('ts-node').register({
-            project: paths.tsConfigPath,
-            transpileOnly: true,
-            require: ['tsconfig-paths/register']
+        register({
+          project: paths.tsConfigPath,
+          transpileOnly: true,
+          require: ['tsconfig-paths/register']
         });
+        
     }
 
     async loadMigrationFile(importPath: string): Promise<Migration> {
