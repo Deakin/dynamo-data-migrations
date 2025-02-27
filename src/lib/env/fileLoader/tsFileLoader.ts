1d0aaa54078dcd31a5e4cde7be99f47c8757801b
Anthony George <anthony.george@deakin.edu.au>
2025-02-27 11:26:55 +1100

    chore: register ts-nodem

--- src/lib/env/fileLoader/tsFileLoader.ts
+++ src/lib/env/fileLoader/tsFileLoader.ts
@@ -1,14 +1,22 @@
-import { pathToFileURL } from 'url';
 import { FileLoader, Migration } from './fileLoader';
 import * as paths from '../paths';
-import 'ts-node/esm';
+import { pathToFileURL } from 'url';
+import { register } from 'ts-node';
 
 export class TsFileLoader extends FileLoader {
     constructor() {
         super(paths.tsExtension, paths.tsMigrationPath);
+
+        console.log('Registering ts-node...'); // Debugging log
+        register({
+            transpileOnly: true,
+            project: paths.tsConfigPath, // Ensure this is set
+            require: ['tsconfig-paths/register'],
+        });
     }
 
     async loadMigrationFile(importPath: string): Promise<Migration> {
+        console.log('Importing migration file:', importPath); // Debugging log
         return import(pathToFileURL(importPath).href);
     }
 }
